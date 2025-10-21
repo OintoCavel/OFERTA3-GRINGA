964f7d0c713da8a73dc869b14f046985881fc27a
Rafa <Rafaellucenazika@gmail.com>
2025-10-21 23:28:02 +0000

    <script>   window.pixelId = "68f816d0a854355c218fa393";   var a = docume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,25 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&display=swap" rel="stylesheet" />
+        <script
+          id="utmify-pixel-config"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "68f816d0a854355c218fa393";
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script
+          id="utmify-pixel-script"
+          src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
+          strategy="afterInteractive"
+          async
+          defer
+        />
       </body>
     </html>
   );
